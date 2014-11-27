efe6034c545420103c0390c1347c5c81d7dc4485
suwapong <cappza@msn.com>
2014-11-27 23:18:39 +0700

    MvcApplication2

---
+++ MvcApplication2/Controllers/CarController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication2.Models;
+
+namespace MvcApplication2.Controllers
+{
+    public class CarController : Controller
+    {
+        private CarDBContext db = new CarDBContext();
+
+        //
+        // GET: /Car/
+
+        public ActionResult Index()
+        {
+            return View(db.Car.ToList());
+        }
+
+        //
+        // GET: /Car/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Car car = db.Car.Find(id);
+            if (car == null)
+            {
+                return HttpNotFound();
+            }
+            return View(car);
+        }
+
+        //
+        // GET: /Car/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Car/Create
+
+        [HttpPost]
+        public ActionResult Create(Car car)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Car.Add(car);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(car);
+        }
+
+        //
+        // GET: /Car/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Car car = db.Car.Find(id);
+            if (car == null)
+            {
+                return HttpNotFound();
+            }
+            return View(car);
+        }
+
+        //
+        // POST: /Car/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Car car)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(car).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(car);
+        }
+
+        //
+        // GET: /Car/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Car car = db.Car.Find(id);
+            if (car == null)
+            {
+                return HttpNotFound();
+            }
+            return View(car);
+        }
+
+        //
+        // POST: /Car/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Car car = db.Car.Find(id);
+            db.Car.Remove(car);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MvcApplication2/Models/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace MvcApplication2.Models
+{
+    public class Car
+   
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }        
+        public DateTime ReleaseDate { get; set; }        
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class CarDBContext : DbContext
+    {
+        public DbSet<Car> Car { get; set; }
+    }
+}
